1a7308786b22d49e33aa647f2974e37033d55561
yerisaguilar <yerisaguilarg@gmail.com>
2023-09-19 10:46:24 -0700

    updated transition animation

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,17 @@
-import React, {useRef } from 'react'
+import React, {useRef, useState } from 'react'
 import Sliders from './Slider'
 
 const Tech = ({sectionRefs, tech}) => {
+    const [readMore, setReadMore] = useState(false);
+
     const {title, subtitle, paragraph,description, technologies} = tech
  
     const techRef = useRef(null);
     sectionRefs.current['tech'] = techRef;
+
+    const handleOnClick =()=>{
+        setReadMore(!readMore);
+    }
     return (
     
     <section id="tech" className="bg-gray" ref={techRef}>
@@ -15,7 +21,8 @@ const Tech = ({sectionRefs, tech}) => {
                     <h4 className="text-white text-shadow">{title}</h4>
                     <h3 className="text-blue ">{subtitle}{` `}
                     <span className="text-white ">{paragraph}</span></h3>
-                    <p className="text-white description-text">{description}</p>
+                    <p className={`text-white description-text ${readMore? '':'elipsis'} `}>{description}</p>
+                    <button className="btn btn-outline-info btn-sm" onClick={handleOnClick}>{`${readMore? 'Read Less':'Read More'}`}</button>
                 </div>
                  <div className="col-md-12 col-lg-6 p-0">
                     <div className="tech-slider ">
