bcc15d3f21eba995f0ded51e2b5de74c47568291
yerish14 <yerisagui@gmail.com>
2023-12-27 17:39:47 -0800

    added tailwind styles

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,37 +1,30 @@
 <template>
   <div>
     <footer class="footer">
-      <div class="container footer-container">
-        <div class=" ">
+      <div class="container flex">
+        <div class="w-1/3">
           <p class="uppercase">Front End web Developer</p>
-          <h5>Yeris AlejanDra Aguilar</h5>
+          <p class="text-2xl capitalize">Yeris Alejandra Aguilar</p>
           <p><i class="location"></i>Tijuana, MX</p>
 
-          <ul class="social">
-            <li>
-              <a href="" class="linkedin"></a>
-            </li>
-            <li>
-              <a href="" class="email"></a>
-            </li>
-            <li>
-              <a href="" class="phone"></a>
-            </li>
-            <li>
-              <a href="" class="github"></a>
+          <ul class="social mt-3">
+            <li v-for="link in links" :key="link.alt">
+              <a :href="link.url" class="linkedin">
+                <img :src="link.icon" :alt="link.alt"
+              /></a>
             </li>
           </ul>
         </div>
-        <div class="quick-links-cont display-desktop">
+        <div class="flex-col justify-center items-center hidden md:flex w-1/3">
           <ul class="quick-links">
-            <li><a href="">home</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">About me</a></li>
+            <li v-for="nav in navigation" :key="nav.name">
+              <a :href="nav.href">{{ nav.name }}</a>
+            </li>
           </ul>
         </div>
-        <div class="contact-footer display-desktop">
-          <p>Get Started TODAY!</p>
-          <a href="" class="btn-white"> contact me</a>
+        <div class="hidden md:block w-1/3">
+          <p class="text-2xl">Get Started TODAY!</p>
+          <a href="" class="btn-white mt-2"> contact me</a>
         </div>
       </div>
     </footer>
@@ -39,3 +32,17 @@
     <NavFloatingNav />
   </div>
 </template>
+<script setup>
+const navigation = [
+  { name: "Home", href: "#hero", current: true },
+  { name: "About", href: "#about", current: false },
+  { name: "Projects", href: "#projects", current: false },
+];
+
+const links = [
+  { url: "", icon: "/assets/linkedIn.svg", alt: "LinkedIn icon" },
+  { url: "", icon: "/assets/email.svg", alt: "Email icon" },
+  { url: "", icon: "/assets/phone.svg", alt: "Phone icon" },
+  { url: "", icon: "/assets/github.svg", alt: "Github icon" },
+];
+</script>
--- components/Nav/FloatingNav.vue
+++ components/Nav/FloatingNav.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
-    <div class="links">
+    <div class="links flex">
       <div>
         <!-- contact links -->
-        <ul>
-          <li>
-            <a href="" class="linkedin"></a>
-          </li>
-          <li>
-            <a href="" class="email"></a>
-          </li>
-          <li>
-            <a href="" class="phone"></a>
-          </li>
-          <li>
-            <a href="" class="github"></a>
+        <ul class="flex flex-col gap-y-4">
+          <li v-for="link in links" :key="link.alt">
+            <a :href="link.url">
+              <img :src="link.icon" :alt="link.alt" />
+            </a>
           </li>
         </ul>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+const links = [
+  { url: "", icon: "/assets/linkedIn.svg", alt: "LinkedIn icon" },
+  { url: "", icon: "/assets/email.svg", alt: "Email icon" },
+  { url: "", icon: "/assets/phone.svg", alt: "Phone icon" },
+  { url: "", icon: "/assets/github.svg", alt: "Github icon" },
+];
+</script>
+<style>
+.links {
+  border-radius: 0px 20px 50px 0px;
+  background: #a15aff;
+  position: fixed;
+  top: 20%;
+  padding: 33px 19px;
+}
+</style>
