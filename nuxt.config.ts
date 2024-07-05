17b3e6f860df2360bfa7def4daf2253cc9ad4f53
yerisaguilar <yerisaguilarg@gmail.com>
2024-07-05 10:58:54 -0700

    changed work flow and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  // plugins: ["~/plugins/scroll.ts"],
+  target: "static",
+
+  router: {
+    base: "/portfolio/",
+  },
+  components: true,
+  buildModules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
+
   modules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
   css: ["~/assets/css/style.css"],
-  // plugins: ["~/plugins/scroll.ts"],
-target:'static',
-
-buildModules: [
-  "@nuxtjs/tailwindcss", "nuxt-swiper"
-],
-  router: {
-    base: "/<portfolio>/",
-  },
-
   generate: {
     fallback: true,
   },
   // Public runtime configuration
   publicRuntimeConfig: {
-    baseURL: process.env.NUXT_APP_BASE_URL || 'http://localhost:3000'
-  }
+    baseURL: process.env.NUXT_APP_BASE_URL || "http://localhost:3000",
+  },
+  devtools: { enabled: true },
 });
