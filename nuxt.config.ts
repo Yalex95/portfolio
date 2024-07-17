5ce9da0c7f3272538061c0ce06d3f879fa3c60c2
yerisaguilar <yerisaguilarg@gmail.com>
2024-07-17 16:36:08 -0700

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,30 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// In nuxt.config.js or in a component
-// import 'swiper/swiper-bundle.css';
+
 
 export default defineNuxtConfig({
-  // plugins: ["~/plugins/scroll.ts"],
-  target: "static",
+  // target: "static",
 
-  router: {
-    base: "/portfolio/",
-  },
+  // router: {
+  //   base: "/portfolio/",
+  // },
   components: true,
   buildModules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
 
   modules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
   css: ["~/assets/css/style.css"],
-  generate: {
-    fallback: true,
-    exclude: [
-      /^\/200/,
-      /^\/404/, // Exclude routes like /200 from being generated if it's not a real page
-    ],
+  nitro: {
+    firebase: {
+      gen: 2
+    }
   },
-  build: {
-    publicPath: "/portfolio/_nuxt/",
-  },
-  // Public runtime configuration
-  // publicRuntimeConfig: {
-  //   baseURL: process.env.NUXT_APP_BASE_URL || "http://localhost:3000",
+  // generate: {
+  //   fallback: true,
+  //   exclude: [
+  //     /^\/200/,
+  //     /^\/404/, // Exclude routes like /200 from being generated if it's not a real page
+  //   ],
+  // },
+  // build: {
+  //   publicPath: "/portfolio/_nuxt/",
   // },
   devtools: { enabled: true },
 });
